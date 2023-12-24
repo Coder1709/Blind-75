1b500192e1505f957689aa9a4d7cc06183d2c0d4
Arpit Pathak <110691809+Coder1709@users.noreply.github.com>
2023-12-24 21:47:17 +0530

    Duplicate Done

--- Duplicate.cpp
+++ Duplicate.cpp
@@ -26,3 +26,75 @@ public:
 };
 
 //Time Limit Exceed
+
+//second solution using set 
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+
+        unordered_set <int> l;
+
+        bool ans = true;
+
+        for ( int i = 0 ; i < n ; i++) {
+            l.insert(nums[i]);
+        }
+
+        int m = l.size();
+
+
+        if(  n == m) {
+            ans = false;
+        }
+
+        return ans;
+
+        
+    }
+};
+
+
+//using inbuild function
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        unordered_set <int> l;
+        for ( int i = 0 ; i < nums.size();  i++) {
+
+            if( l.find(nums[i]) != l.end() ) {
+                return true;
+            }
+            l.insert(nums[i]);
+        }
+
+        return false;
+
+    
+
+        
+    }
+};
+
+
+
+//javascript solution
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let set = new Set();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (set.has(nums[i])) {
+            return true; // Found a duplicate
+        }
+        set.add(nums[i]);
+    }
+
+    return false; // No duplicates found
+};
